cb3e27b2276f980940992505d0a43e23acb68fbb
Mayur <mayur.saitwal@gmail.com>
2024-04-24 20:25:53 +0530

    first

---
+++ Assets/SweetSugar/Scripts/System/EffectScene.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SweetSugar.Scripts.System
+{
+	public class EffectScene : MonoBehaviour
+	{
+
+		// Use this for initialization
+		void Start()
+		{
+			SceneManager.LoadScene("game");
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+	}
+}
